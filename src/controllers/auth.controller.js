9aebc02172e0ff4c0f67f9e659b6e79081b4be79
omaraliii1 <omaraliyassin18@gmail.com>
2025-05-09 15:13:00 +0300

    move controllers to public controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -137,36 +137,11 @@ async function editUser(req, res) {
   }
 }
 
-function notAuth(req, res) {
-  res.status(200).json({
-    message: "Welcome. You are not authenticated.",
-  });
-}
-
-async function auth(req, res) {
-  const userId = req.params.id;
-  try {
-    const user = await User.findById(userId);
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    res.status(200).json({
-      message: "Authenticated user",
-      userId,
-      name: user.username,
-      role: user.role,
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Server error" });
-  }
-}
-
 module.exports = {
   register,
   login,
   logout,
   deleteUser,
-  notAuth,
-  auth,
   getUserById,
   getAllUsers,
   editUser,
