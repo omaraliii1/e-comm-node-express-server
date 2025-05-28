7deca25626f7c472fb65214c9b806f0efc2c300f
omaraliii1 <omaraliyassin18@gmail.com>
2025-05-28 16:37:41 +0300

    change the return value of the notAuth function

--- src/users/controller/public.controller.ts
+++ src/users/controller/public.controller.ts
@@ -5,8 +5,13 @@ import { IUser } from "../../users/interfaces/user.interface";
 import { createBaseError } from "../../utils/baseErrorHandler";
 import { BaseResponseHandler } from "../../utils/baseResponseHandler";
 
-export const notAuth = (req: Request, res: Response): void => {
-  res.status(200).json("Welcome to the Home Page!");
+export const notAuth = (
+  req: Request,
+  res: Response<BaseResponse<string>>
+): void => {
+  res
+    .status(200)
+    .json(BaseResponseHandler.create(200, "Welcome to the Home Page", ""));
 };
 
 export const auth = async (
